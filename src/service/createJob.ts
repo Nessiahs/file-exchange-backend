a28db82f564443fd83551f864996eeabdb720220
Konstantin Kroner <konstantin.kroner@smbag.de>
2021-06-12 12:54:24 +0200

    create job generate secrets and expire time will calculated

--- src/service/createJob.ts
+++ src/service/createJob.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from "express";
+import { generate } from "generate-password";
+import moment from "moment";
 import { v4 as uuidv4 } from "uuid";
 import { STATUS_CODES } from "../config/statusCodes";
 import { insertJob } from "../db/insertJob";
 type TBodyData = {
   jobType: "download" | "upload";
   jobName: string;
-  password?: string;
   expires?: string;
 };
 
 export const createJob = async (req: Request, res: Response) => {
   const token = uuidv4();
-  const { jobType, jobName, password, expires } = req.body as TBodyData;
+
+  const { jobType, jobName, expires } = req.body as TBodyData;
+  const { id: createdBy } = req.body.tokenData;
+  const secret = generate({
+    length: 8,
+    numbers: true,
+  });
+
+  const expireDate = moment().add(expires, "h");
 
   const link = `${process.env.FRONTEND_URI ?? `https://${req.headers.host}/`}${
     process.env.FRONTEND_UPLOAD_PATH ?? "upload/"
@@ -21,12 +30,12 @@ export const createJob = async (req: Request, res: Response) => {
     await insertJob({
       jobType,
       jobName,
-      secret: password,
-      expires,
+      secret,
+      expires: expireDate.format("YYYY-MM-DD HH:mm:ss"),
       token,
-      createdBy: req.body.tokenData.id,
+      createdBy,
     });
-    res.send({ link, token });
+    res.send({ link, token, secret });
   } catch (error) {
     res.status(STATUS_CODES.ServerError).send();
   }
